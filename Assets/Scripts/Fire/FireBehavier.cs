f19e9a52a5b3cc8e898beba60f93ae2a6e3184dc
yangminxing <ymxbtbu@163.com>
2022-08-22 10:19:41 +0800

    // ENCAPSULATION

---
+++ Assets/Scripts/Fire/FireBehavier.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBehavier : MonoBehaviour
+{
+    // define who open fire
+    private AbstractUnit m_source;
+
+    //  define who get fired
+    private AbstractUnit m_target;
+
+    protected float moveSpeed = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireTo(AbstractUnit unit) {
+        gameObject.transform.Translate((m_target.transform.position - m_source.transform.position).normalized * Time.deltaTime * moveSpeed);
+    }
+}
