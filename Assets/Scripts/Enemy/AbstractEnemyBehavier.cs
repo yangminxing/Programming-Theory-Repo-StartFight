3bb2ad8ec276c8be4893aa207c51150ccbad7995
yangminxing <ymxbtbu@163.com>
2022-08-22 10:20:46 +0800

    // POLYMORPHISM

---
+++ Assets/Scripts/Enemy/AbstractEnemyBehavier.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractEnemyBehavier : AbstractUnit
+{
+    private float spwanYPos = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector3()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
